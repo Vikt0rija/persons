e7fe0587fe3cc9a65053795a6bec4f93bb337ed0
Viktorija <84173635+Vikt0rija@users.noreply.github.com>
2021-08-30 13:50:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var arr = ["Jon Snow", "Cersei Lannister", "Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"];
+
+console.log("Sąraše yra " + arr.length + ".");
+console.log("Jon Snow index sąraše yra " + arr.indexOf("John Snow"));
+// console.log("Cersei Lannister index sąraše yra " + arr.indexOf("Cersei Lannister"));
+// console.log("Daenerys Targaryen index sąraše yra " + arr.indexOf("Daenerys Targaryen"));
+// console.log("Theon Greyjoy index sąraše yra " + arr.indexOf("Theon Greyjoy"));
+// console.log("Tyrion Lannister index sąraše yra " + arr.indexOf("Tyrion Lannister"));
+console.log("Arya Stark index sąraše yra " + arr.indexOf("Arya Stark"));
+
+
+
+  var name = prompt("Įveskite skaičių nuo 1 iki 6");
+  if (name == 1){
+  console.log("Personažas yra John Snow");}
+
+  if (name == 2)
+  {console.log("Personažas yra Cersei Lannister");}
+
+  if (name == 3)
+  {console.log("Personažas yra Daenerys Targaryen");}
+
+  if (name == 4)
+  {console.log("Personažas yra Theon Greyjoy");}
+
+  if (name == 5)
+  {console.log("Personažas yra Tyrion Lannister");}
+
+  if (name == 6)
+  {console.log("Personažas yra Arya Stark");}
